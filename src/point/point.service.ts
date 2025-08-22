7759b569b5f788348ae67afeef3f27aac4241e23
Hansol <losnah24@gmail.com>
2025-08-22 17:53:19 +0900

    :necktie: Point 비즈니스 로직 구현

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserPointTable } from '../database/userpoint.table';
 import { PointHistoryTable } from '../database/pointhistory.table';
-import { PointHistory, UserPoint } from './point.model';
+import { PointHistory, TransactionType, UserPoint } from './point.model';
 
 @Injectable()
 export class PointService {
@@ -11,23 +11,33 @@ export class PointService {
   ) {}
 
   async point(userId: number): Promise<UserPoint> {
-    // return this.userDb.selectById(userId);
-    return { id: userId, point: 0, updateMillis: Date.now() };
+    return this.userDb.selectById(userId);
   }
 
   async history(userId: number): Promise<PointHistory[]> {
-    // return this.historyDb.selectAllByUserId(userId);
-    console.log(userId);
-    return [];
+    return this.historyDb.selectAllByUserId(userId);
   }
 
   async charge(userId: number, amount: number): Promise<UserPoint> {
-    // return this.userDb.insertOrUpdate(userId, amount);
-    return { id: userId, point: amount, updateMillis: Date.now() };
+    const currentUserPoint = await this.userDb.selectById(userId);
+    const [updatedUserPoint] = await Promise.all([
+      this.userDb.insertOrUpdate(userId, currentUserPoint.point + amount),
+      this.historyDb.insert(userId, amount, TransactionType.CHARGE, Date.now()),
+    ]);
+    return updatedUserPoint;
   }
 
   async use(userId: number, amount: number): Promise<UserPoint> {
-    // return this.userDb.insertOrUpdate(userId, amount);
-    return { id: userId, point: amount, updateMillis: Date.now() };
+    const currentUserPoint = await this.userDb.selectById(userId);
+
+    if (currentUserPoint.point < amount) {
+      throw new BadRequestException('Not enough point');
+    }
+
+    const [updatedUserPoint] = await Promise.all([
+      this.userDb.insertOrUpdate(userId, currentUserPoint.point - amount),
+      this.historyDb.insert(userId, amount, TransactionType.USE, Date.now()),
+    ]);
+    return updatedUserPoint;
   }
 }
